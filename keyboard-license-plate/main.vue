8e15e7beec00407e9561b56cdb8c278d8b809096
jackyang <ibca@users.noreply.github.com>
2022-01-19 09:37:33 +0800

    Update main.vue

--- keyboard-license-plate/main.vue
+++ keyboard-license-plate/main.vue
@@ -84,7 +84,7 @@ const letter = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ''],
   ['', '', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '', ''],
-  ['', '', '挂','学', '警', '领', '使', '', '']
+   ['', '', '', '挂', '学', '警', '领', '使', '', '']
 ]
 const provinceFormat = flat(province)
 const letterFormat = flat(letter)
